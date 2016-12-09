1d6ed6fdab47b6a9d1840fdc8d3ca1819e00455a
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-12-08 23:19:06 -0800

    Initial Commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
-
-import RocketList from './rockets/RocketList';
-import RocketForm from './rockets/RocketForm';
+import update from 'immutability-helper';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    this.socket = io.connect('http://192.168.1.19:3001');
+    this.socket.on('receive message', data => {
+      this.setState(update(this.state, {
+        messages: {
+          $push: [data.message]
+        }
+      }));
+    });
+  }
+
+  update = name => e => {
+    this.setState({ [name]: e.target.value });
+  }
+
+  send = () => {
+    this.socket.emit('new message', { message: this.state.message});
+    this.setState({ message: '' });
+  }
 
   render() {
     return (
       <div>
-        <h1>Welcome to React Apollo Fullstack Skeleton!</h1>
-        <RocketForm />
-        <RocketList />
+        <h1>Messages</h1>
+        <input type="text" onChange={this.update('message')} value={this.state.message} />
+        <button onClick={this.send}>Send</button>
+        <ul>
+          {this.state.messages.map((message, idx) => (
+            <li key={idx}>{message}</li>
+          ))}
+        </ul>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,37 @@
 import express from 'express';
+import http from 'http';
 import bodyParser from 'body-parser';
 import path from 'path';
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
-
-import typeDefs from './data/schema';
-import resolvers from './data/resolvers';
-
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+import socketio from 'socket.io';
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 if (process.env.NODE_ENV === 'production') {
   app.use('/static', express.static('dist/client'));
-  app.get('*', function(req, res) {
+  app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../../index.html'));
   });
 } else {
   const cors = require('cors');
   app.use(cors());
-  app.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql'
-  }));
 }
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.get('/test', (req, res) => {
+  res.send('hello');
+});
 
+io.on('connection', socket => {
+  console.log('someone connected');
+  socket.join('test room');
+  socket.on('new message', data => {
+    io.to('test room').emit('receive message', data);
+  });
+});
 
 var PORT = process.env.PORT || 3001;
-app.listen(PORT, function(err) {
+server.listen(PORT, '0.0.0.0', function(err) {
   if (err) return console.error(err);
   console.log('Listening on port', PORT);
 });
